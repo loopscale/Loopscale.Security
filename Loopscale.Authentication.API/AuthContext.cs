ef39d8188b73209dd6b62fcd3e85831a77c0be71
Loopscale <support@loopscale.com>
2016-09-26 12:00:13 -0400

    Added the comment

--- Loopscale.Authentication.API/AuthContext.cs
+++ Loopscale.Authentication.API/AuthContext.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
+//Database context..
 namespace Loopscale.Authentication.API
 {
     public class AuthContext : IdentityDbContext<IdentityUser>
